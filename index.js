bdbd702178bc3bd5e95e87a24c758972ed3fad52
438417623wq <438417623@qq.com>
2026-07-07 15:02:45 +0800

    Reset GitHub upload to local build v0.6.24

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
     'use strict';
 
     const NAMESPACE = 'AIWorldbookRouter';
-    const VERSION = '0.6.17';
+    const VERSION = '0.6.24';
     const LOG_PREFIX = '[AI Worldbook Router Bootstrap]';
     const ENTRY_ID = 'ai_wbr_extension_entry';
     const ROW_ID = 'ai_wbr_extension_row';
